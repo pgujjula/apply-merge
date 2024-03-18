4dc260f9115c30ac1627acd55707f73f6aab8432
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-03-18 15:00:10 -0700

    Add benchmark suite
    Add a benchmark suite for the applyMerge implementations, based on
    tasty-bench.

---
+++ bench/Main.hs
@@ -0,0 +1,37 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+
+module Main (main) where
+
+import Data.Function ((&))
+import Data.List.ApplyMerge.IntMap qualified
+import Data.List.ApplyMerge.IntSet qualified
+import Test.Tasty.Bench (Benchmark, bench, bgroup, defaultMain, nf)
+
+main :: IO ()
+main =
+  defaultMain
+    [ benchCommon "IntMap" Data.List.ApplyMerge.IntMap.applyMerge,
+      benchCommon "IntSet" Data.List.ApplyMerge.IntSet.applyMerge
+    ]
+
+benchCommon ::
+  String ->
+  (forall a b c. (Ord c) => (a -> b -> c) -> [a] -> [b] -> [c]) ->
+  Benchmark
+benchCommon name applyMerge =
+  bgroup name [benchmarkSymmetric]
+  where
+    benchmarkSymmetric :: Benchmark
+    benchmarkSymmetric =
+      bgroup "benchmarkSymmetric" (fmap mkBench [1 .. (6 :: Int)])
+      where
+        mkBench :: Int -> Benchmark
+        mkBench i = bench (show i) (nf collapse (10 ^ i))
+          where
+            collapse :: Int -> Int
+            collapse n =
+              let start = (n `quot` maxBound) + 1
+               in applyMerge (*) [start ..] [start ..]
+                    & take n
+                    & sum
