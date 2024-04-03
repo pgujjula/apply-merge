08de48681f507d2ee51e1c0ace5c598a90be9dcc
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-04-02 18:36:26 -0700

    Set IntSet-based applyMerge as the default
    The IntSet-based applyMerge is now as fast as the IntMap-based
    applyMerge, and is a simpler implementation. So we set it as the default
    applyMerge.

--- src/Data/List/ApplyMerge.hs
+++ src/Data/List/ApplyMerge.hs
@@ -3,7 +3,7 @@
 
 module Data.List.ApplyMerge (applyMerge) where
 
-import Data.List.ApplyMerge.IntMap qualified
+import Data.List.ApplyMerge.IntSet qualified
 
 applyMerge :: (Ord c) => (a -> b -> c) -> [a] -> [b] -> [c]
-applyMerge = Data.List.ApplyMerge.IntMap.applyMerge
+applyMerge = Data.List.ApplyMerge.IntSet.applyMerge
